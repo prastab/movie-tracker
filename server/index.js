14430c10b7bc86e7b2432786396c74820ae98fda
Prastab <prastabg16@gmail.com>
2025-03-16 17:04:55 -0500

    testing deployment

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public", "dist")));
-
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
 // Database connection pool
 pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
